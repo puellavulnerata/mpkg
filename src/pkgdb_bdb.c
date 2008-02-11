8e61cecbca586ced9aff665217e89cf2b8b5b266
Dana Koch <impatiens@gmail.com>
2008-02-11 10:00:56 +0000

    style cleanups plus some minor style stuff

--- src/pkgdb_bdb.c
+++ src/pkgdb_bdb.c
@@ -6,30 +6,30 @@
 pkg_db * open_pkg_db_bdb( char *filename ) {
   pkg_db *ret = malloc(sizeof struct pkg_db);
 
-  if(ret == NULL) 
-    perror("");
+  if( ret == NULL ) 
+    perror( "" );
 
   ret->query = query_bdb;
   ret->insert = insert_into_bdb;
   ret->delete = delete_from_bdb;
   ret->close = close_bdb;
 
-  if(db_create(db->private, NULL, 0) != 0)
+  if( db_create( db->private, NULL, 0 ) != 0 )
   {
     // Might want to use DB->err, but it's simpler to fprintf.
-    fprintf(stderr, "bdb database create failed\n");
-    free(ret);
+    fprintf( stderr, "bdb database create failed\n" );
+    free( ret );
     return NULL;
   }
 
-  return db->open(db, NULL, filename, NULL, DB_BTREE, DB_CREATE, 0);
+  return db->open( db, NULL, filename, NULL, DB_BTREE, DB_CREATE, 0 );
 }
 
 int close_bdb( pkg_db *db ) {
   int ret;
   DB *bdb = db->private;
-  ret = bdb->close(bdb, 0); // frees db->private too
-  free(db);
+  ret = bdb->close( bdb, 0 ); // frees db->private too
+  free( db );
   return ret;
 }
 
@@ -40,7 +40,7 @@ int delete_from_bdb( pkg_db *db, char *key ) {
   bdb_key.data = key;
   bdb_key.size = SIZEOF_STR( key );
 
-  return bdb->del( bdb, NULL, &bdb_key, 0);
+  return bdb->del( bdb, NULL, &bdb_key, 0 );
 }
 
 char * query_bdb( pkg_db *db, char *key ) {
@@ -53,7 +53,7 @@ char * query_bdb( pkg_db *db, char *key ) {
 
   bdb->get( bdb, NULL, &bdb_key, &bdb_value, 0 );
 
-  ret = malloc( ( bdb_value.size + 1 ) * sizeof( char ) );
+  ret = malloc( SIZEOF_STR(bdb_value.data) );
   strcpy( ret, bdb_data.data );
   
   return ret;
@@ -69,5 +69,5 @@ int insert_into_bdb( pkg_db *db, char *key, char *value ) {
   bdb_value.data = value;
   bdb_value.size = SIZEOF_STR( value );
 
-  return bdb->put( bdb, NULL, &key, &value, 0);
+  return bdb->put( bdb, NULL, &key, &value, 0 );
 }
