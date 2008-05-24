d0309c292d4fcc345ec98cf42be05f897001f319
Andrea Shepard <andrea@persephoneslair.org>
2008-05-24 09:38:48 +0000

    Fixed memory leak

--- src/compression.c
+++ src/compression.c
@@ -1,11 +1,17 @@
 #include <pkg.h>
 
 void close_read_stream( read_stream *r ) {
-  if ( r ) r->close( r->private );
+  if ( r ) {
+    r->close( r->private );
+    free( r );
+  }
 }
 
 void close_write_stream( write_stream *w ) {
-  if ( w ) w->close( w->private );
+  if ( w ) {
+    w->close( w->private );
+    free( w );
+  }
 }
 
 long read_from_stream( read_stream *r, void *buf, long len ) {
