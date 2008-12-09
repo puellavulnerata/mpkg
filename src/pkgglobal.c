a53610e14183c832aa0149736ddc3d7c10421b96
Andrea Shepard <andrea@persephoneslair.org>
2008-12-09 06:55:57 +0000

    Canonicalize paths when setting them in pkgglobal.c

--- src/pkgglobal.c
+++ src/pkgglobal.c
@@ -30,11 +30,21 @@ const char * get_pkg( void ) {
 }
 
 void set_pkg( const char *pkg_in ) {
+  int status;
+
   if ( pkg_in ) {
     if ( pkg && pkg != DEFAULT_PKG_STRING ) free( pkg );
     pkg = malloc( sizeof( *pkg ) * ( strlen( pkg_in ) + 1 ) );
     if ( pkg ) {
       strcpy( pkg, pkg_in );
+      status = canonicalize_path( pkg );
+      if ( status != 0 ) {
+	fprintf( stderr,
+		 "Warning: couldn't canonicalize while setting pkgdir to \"%s\".\n",
+		 pkg_in );
+	free( pkg );
+	pkg = DEFAULT_PKG_STRING;
+      }
     }
     else {
       fprintf( stderr,
@@ -51,11 +61,21 @@ const char * get_root( void ) {
 }
 
 void set_root( const char *root_in ) {
+  int status;
+
   if ( root_in ) {
     if ( root && root != DEFAULT_ROOT_STRING ) free( root );
     root = malloc( sizeof( *root ) * ( strlen( root_in ) + 1 ) );
     if ( root ) {
       strcpy( root, root_in );
+      status = canonicalize_path( root );
+      if ( status != 0 ) {
+	fprintf( stderr,
+		 "Warning: couldn't canonicalize while setting installroot to \"%s\".\n",
+		 root_in );
+	free( root );
+	root = DEFAULT_ROOT_STRING;
+      }
     }
     else {
       fprintf( stderr,
@@ -72,11 +92,21 @@ const char * get_temp( void ) {
 }
 
 void set_temp( const char *temp_in ) {
+  int status;
+
   if ( temp_in ) {
     if ( temp && temp != DEFAULT_TEMP_STRING ) free( temp );
     temp = malloc( sizeof( *temp ) * ( strlen( temp_in ) + 1 ) );
     if ( temp ) {
       strcpy( temp, temp_in );
+      status = canonicalize_path( temp );
+      if ( status != 0 ) {
+	fprintf( stderr,
+		 "Warning: couldn't canonicalize while setting tempdir to \"%s\".\n",
+		 temp_in );
+	free( temp );
+	temp = DEFAULT_TEMP_STRING;
+      }
     }
     else {
       fprintf( stderr,
