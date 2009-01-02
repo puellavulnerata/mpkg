f64d4b02ace105623553a07b72f82838d631e3b9
Andrea Shepard <andrea@persephoneslair.org>
2009-01-02 05:55:13 +0000

    Don't depend on directory existence in pkgglobal.c

--- src/pkgglobal.c
+++ src/pkgglobal.c
@@ -4,6 +4,31 @@ static char *pkg = NULL;
 static char *root = NULL;
 static char *temp = NULL;
 
+static char * adjust_path( const char * );
+
+static char * adjust_path( const char *path_in ) {
+  char *adjusted;
+  char *cwd;
+
+  adjusted = NULL;
+  if ( path_in ) {
+    if ( is_absolute( path_in ) ) {
+      adjusted = canonicalize_and_copy( path_in );
+    }
+    else {
+      cwd = get_current_dir();
+      if ( cwd ) {
+	adjusted = concatenate_paths( cwd, path_in );
+	free( cwd );
+      }
+      /* else error, return NULL */
+    }
+  }
+  /* else error, return NULL */
+
+  return adjusted;
+}
+
 void free_pkg_globals( void ) {
   if ( pkg && pkg != DEFAULT_PKG_STRING ) {
     free( pkg );
@@ -30,40 +55,15 @@ const char * get_pkg( void ) {
 }
 
 void set_pkg( const char *pkg_in ) {
-  int status, result;
-  char *old_cwd;
+  char *tmp;
 
   if ( pkg_in ) {
-    old_cwd = get_current_dir();
-    if ( old_cwd ) {
-      result = chdir( pkg_in );
-      if ( result == 0 ) {
-	if ( pkg && pkg != DEFAULT_PKG_STRING ) free( pkg );
-	pkg = get_current_dir();
-	if ( pkg ) {
-	  status = canonicalize_path( pkg );
-	  if ( status != 0 ) {
-	    fprintf( stderr,
-		     "Warning: couldn't canonicalize while setting pkgdir to \"%s\".\n",
-		     pkg_in );
-	    free( pkg );
-	    pkg = DEFAULT_PKG_STRING;
-	  }
-	}
-	else {
-	  fprintf( stderr,
-		   "Warning: get_current_dir() failed while setting pkgdir.\n" );
-	  pkg = DEFAULT_PKG_STRING;
-	}
-	chdir( old_cwd );
-      }
-      free( old_cwd );
+    tmp = adjust_path( pkg_in );
+    if ( tmp ) {
+      if ( pkg && pkg != DEFAULT_PKG_STRING ) free( pkg );
+      pkg = tmp;
     }
-    else {
-      fprintf( stderr,
-	       "Warning: get_current_dir() failed while setting pkgdir.\n" );
-      pkg = DEFAULT_PKG_STRING;
-    }
+    else pkg = DEFAULT_PKG_STRING;
   }
   else pkg = DEFAULT_PKG_STRING;
 }
@@ -73,40 +73,15 @@ const char * get_root( void ) {
 }
 
 void set_root( const char *root_in ) {
-  int status, result;
-  char *old_cwd;
+  char *tmp;
 
   if ( root_in ) {
-    old_cwd = get_current_dir();
-    if ( old_cwd ) {
-      result = chdir( root_in );
-      if ( result == 0 ) {
-	if ( root && root != DEFAULT_ROOT_STRING ) free( root );
-	root = get_current_dir();
-	if ( root ) {
-	  status = canonicalize_path( root );
-	  if ( status != 0 ) {
-	    fprintf( stderr,
-		     "Warning: couldn't canonicalize while setting installroot to \"%s\".\n",
-		     root_in );
-	    free( root );
-	    root = DEFAULT_ROOT_STRING;
-	  }
-	}
-	else {
-	  fprintf( stderr,
-		   "Warning: get_current_dir() failed while setting installroot.\n" );
-	  root = DEFAULT_ROOT_STRING;
-	}
-	chdir( old_cwd );
-      }
-      free( old_cwd );
+    tmp = adjust_path( root_in );
+    if ( tmp ) {
+      if ( root && root != DEFAULT_ROOT_STRING ) free( root );
+      root = tmp;
     }
-    else {
-      fprintf( stderr,
-	       "Warning: get_current_dir() failed while setting installroot.\n" );
-      root = DEFAULT_ROOT_STRING;
-    }
+    else root = DEFAULT_ROOT_STRING;
   }
   else root = DEFAULT_ROOT_STRING;
 }
@@ -116,40 +91,15 @@ const char * get_temp( void ) {
 }
 
 void set_temp( const char *temp_in ) {
-  int status, result;
-  char *old_cwd;
+  char *tmp;
 
   if ( temp_in ) {
-    old_cwd = get_current_dir();
-    if ( old_cwd ) {
-      result = chdir( temp_in );
-      if ( result == 0 ) {
-	if ( temp && temp != DEFAULT_TEMP_STRING ) free( temp );
-	temp = get_current_dir();
-	if ( temp ) {
-	  status = canonicalize_path( temp );
-	  if ( status != 0 ) {
-	    fprintf( stderr,
-		     "Warning: couldn't canonicalize while setting tempdir to \"%s\".\n",
-		     temp_in );
-	    free( temp );
-	    temp = DEFAULT_TEMP_STRING;
-	  }
-	}
-	else {
-	  fprintf( stderr,
-		   "Warning: get_current_dir() failed while setting tempdir.\n" );
-	  temp = DEFAULT_TEMP_STRING;
-	}
-	chdir( old_cwd );
-      }
-      free( old_cwd );
+    tmp = adjust_path( temp_in );
+    if ( tmp ) {
+      if ( temp && temp != DEFAULT_TEMP_STRING ) free( temp );
+      temp = tmp;
     }
-    else {
-      fprintf( stderr,
-	       "Warning: get_current_dir() failed while setting tempdir.\n" );
-      temp = DEFAULT_TEMP_STRING;
-    }
+    else temp = DEFAULT_TEMP_STRING;
   }
   else temp = DEFAULT_TEMP_STRING;
 }
