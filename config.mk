5849ac4c460102f27b4d7e99f0174b4f11b256a4
Andrea Shepard <andrea@persephoneslair.org>
2009-08-10 09:08:11 +0000

    BSD makefile fixes

--- config.mk
+++ config.mk
@@ -8,6 +8,21 @@ CONFIG_BDB=1
 CONFIG_MD5_DEFAULT=1
 CONFIG_MTRACE=0
 
+# Try BDB_INCLUDE=-I/usr/local/include/db4 and BDB_LIBS=-L/usr/local/lib
+# for OpenBSD
+
+BDB_INCLUDE=
+BDB_LIBS=
+
+# Try BZIP2_INCLUDE=-I/usr/local/include and BDB_LIBS=-L/usr/local/lib
+# for OpenBSD
+
+BZIP2_INCLUDE=
+BZIP2_LIBS=
+
+GZIP_INCLUDE=
+GZIP_LIBS=
+
 # Set these to the appropriate commands for your platform, or override them
 # on the command line.
 
@@ -35,10 +50,12 @@ CFLAGS+=-DBUILD_DATE=\"$(shell date +%Y-%m-%d)\"
 
 ifeq ($(CONFIG_BZIP2),1)
 	CFLAGS+=-DCOMPRESSION_BZIP2
+	CFLAGS+=$(BZIP2_INCLUDE)
 endif
 
 ifeq ($(CONFIG_GZIP),1)
 	CFLAGS+=-DCOMPRESSION_GZIP
+	CFLAGS+=$(GZIP_INCLUDE)
 endif
 
 ifeq ($(CONFIG_PKGFMT_V1),1)
@@ -51,6 +68,7 @@ endif
 
 ifeq ($(CONFIG_BDB),1)
 	CFLAGS+=-DDB_BDB
+	CFLAGS+=$(BDB_INCLUDE)
 endif
 
 ifeq ($(CONFIG_MD5_DEFAULT),1)
@@ -66,13 +84,16 @@ endif
 LDFLAGS=
 
 ifeq ($(CONFIG_BDB),1)
+	LDFLAGS+=$(BDB_LIBS)
 	LDFLAGS+=-ldb
 endif
 
 ifeq ($(CONFIG_BZIP2),1)
+	LDFLAGS+=$(BZIP2_LIBS)
 	LDFLAGS+=-lbz2
 endif
 
 ifeq ($(CONFIG_GZIP),1)
+	LDFLAGS+=$(GZIP_LIBS)
 	LDFLAGS+=-lz
 endif
